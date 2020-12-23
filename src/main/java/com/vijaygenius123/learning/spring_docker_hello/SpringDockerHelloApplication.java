a5f9b0c3c820e3817abeb4bf45aaeb321b27da67
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-23 21:14:10 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/learning/spring_docker_hello/SpringDockerHelloApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.spring_docker_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/learning/spring_docker_hello/SpringDockerHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.spring_docker_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
